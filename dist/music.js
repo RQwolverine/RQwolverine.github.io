8cacc827d44c1a7c4533a3e841e099fdc247993a
rq&yl <1151492435@qq.com>
2020-08-31 17:05:46 +0800

    Site updated: 2020-08-31 17:05:23

---
+++ dist/music.js
@@ -0,0 +1,49 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: '斯卡布罗市集',
+        artist: '纯音乐',
+        url: 'http://www.ytmp3.cn/down/47691.mp3',
+        
+      },
+      {
+        name: '喜欢你',
+        artist: 'Beyond',
+        url: 'http://www.ytmp3.cn/down/47493.mp3',
+        
+      },
+	  {
+        name: 'Tessa',
+        artist: 'Steve Jablonsky',
+        url: 'http://www.ytmp3.cn/down/50429.mp3',
+        
+      },
+	  {
+        name: '白兰鸽巡游记',
+        artist: '丢火车乐队',
+        url: 'http://www.ytmp3.cn/down/34837.mp3',
+        
+      },
+      {
+        name: '安妮的仙境',
+        artist: '纯音乐',
+        url: 'http://www.ytmp3.cn/down/47040.mp3',
+        
+      },
+      {
+        name: 'SnowDream',
+        artist: 'bandari',
+        url: 'http://www.ytmp3.cn/down/51791.mp3',
+        
+      },	  
+      {
+        name: '风筝误',
+        artist: '刘珂矣',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      }
+    ]
+});
